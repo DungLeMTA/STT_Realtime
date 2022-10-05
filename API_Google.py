87002673fd85e5703f3381e7e0fe8f05d4d6bf5c
DungLeMTA <duydung.glhd99@gmail.com>
2022-10-05 14:04:11 +0700

    Update

--- API_Google.py
+++
@@ -1,8 +0,0 @@
-from API_STT_a_Dat import api_stt_file
-import os
-i=1
-for file in os.listdir('./sound_save'):
-    file_audio = './sound_save/audio_temp_'+str(i)+'.wav'
-    text = api_stt_file(file_audio)
-    print(i,': ',text)
-    i+=1
